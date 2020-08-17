24dc500832a9e0f04d8ff204018076ae88d79192
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-17 19:39:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.each_with_object({}) do |(key, value), final_array|
-    binding.pry
-    final_array
-  end
-  binding.pry
+  final_results = data.each_with_object({}) do |(key, value), final_array|
+    #binding.pry
+    value.each do |inner_key, names|
+      names.each do |name|
+       # binding.pry
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key)
+        end
+      end
+    end
+  #binding.pry
 end
