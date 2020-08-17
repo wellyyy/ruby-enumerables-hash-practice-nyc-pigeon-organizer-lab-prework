a06adcab953cb4872c2025003f334a9dacda4677
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-17 19:40:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         if !final_array[name][key]
           !final_array[name][key] = []
         end
-        final_array[name][key].push(inner_key)
+        final_array[name][key].push(inner_key.to_s)
         end
       end
     end
